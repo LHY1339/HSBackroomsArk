4b2fbef0ebb3eb69f984600f652bdfe483495c61
LHY1339 <123950909+LHY1339@users.noreply.github.com>
2025-02-05 23:23:40 +0800

    Initial commit

---
+++ Source/HSBackroomsArkServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class HSBackroomsArkServerTarget : TargetRules
+{
+    public HSBackroomsArkServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("HSBackroomsArk");
+	}
+}
